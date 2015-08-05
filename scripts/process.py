7422dcaff01696c30776a190666c17a619f72c6d
Chris Perry <byndcivilization@gmail.com>
2015-08-05 17:50:26 -0400

    SAVEPOINT

--- scripts/process.py
+++ scripts/process.py
@@ -2,14 +2,13 @@ import os
 import urllib
 import logging
 import csv
+import shutil
 
-downloaded = 'cache/bbk_WU5500.csv'
-outpath = 'data/data.csv'
 source_url = 'http://www.bundesbank.de/cae/servlet/StatisticDownload?tsId=BBEX3.M.XAU.USD.EA.AC.C06&its_csvFormat=en&its_fileFormat=csv&mode=its'
 
 def download():
-    if not os.path.exists('cache'):
-        os.makedirs('cache')
+    if not os.path.exists(args.filepath + '/cache'):
+        os.makedirs(args.filepath + '/cache')
     urllib.urlretrieve(source_url, downloaded)
 
 def extract():
@@ -41,19 +40,38 @@ def extract():
     writer = csv.writer(fo)
     writer.writerows(outrows)
     fo.close()
+    shutil.rmtree(args.filepath + '/cache')
 
-import os
-os.environ['http_proxy'] = ''
-def upload():
-    import datastore.client as c
-    dsurl = 'http://datahub.io/dataset/gold-prices/resource/b9aae52b-b082-4159-b46f-7bb9c158d013'
-    client = c.DataStoreClient(dsurl)
-    client.delete()
-    client.upload(outpath)
+# import os
+# os.environ['http_proxy'] = ''
+# def upload():
+#     import datastore.client as c
+#     dsurl = 'http://datahub.io/dataset/gold-prices/resource/b9aae52b-b082-4159-b46f-7bb9c158d013'
+#     client = c.DataStoreClient(dsurl)
+#     client.delete()
+#     client.upload(outpath)
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
+    
+    import argparse
+    parser = argparse.ArgumentParser(
+        description='download and update bundesbank list of monthly gold spot prices')
+
+     # Output file option
+    parser.add_argument('-o', '--output', dest='filepath', action='store',
+                        default=None, metavar='filepath',
+                        help='define output filepath')
+    # # Source file (default is the global cpi_source)
+    # parser.add_argument('source', default=cpi_source, nargs='?',
+    #                     help='source file to generate output from')
+    # Parse the arguments into args
+    args = parser.parse_args()
+
+    downloaded = args.filepath + '/cache/bbk_WU5500.csv'
+    outpath = args.filepath + '/data/data.csv'  
+
     print 'Downloading'
     download()
     print 'Extracting and merging'
